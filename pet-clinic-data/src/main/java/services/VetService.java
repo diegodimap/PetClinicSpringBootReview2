51cd5b0b8386113b5478ff096d18922fe86d26e6
Diego Oliveira <diego.oliveira@bigfishgames.com>
2021-10-29 08:44:24 -0300

    Implemented Interfaces. Closes #1

---
+++ pet-clinic-data/src/main/java/services/VetService.java
@@ -0,0 +1,16 @@
+package services;
+
+import model.Owner;
+import model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet owner);
+
+    Set<Vet> findAll();
+
+}
