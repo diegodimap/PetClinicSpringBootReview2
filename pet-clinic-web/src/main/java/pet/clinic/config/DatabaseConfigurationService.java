3eddb827cfaf0a99a080de13873251c2e00af653
Diego Oliveira <diego.oliveira@bigfishgames.com>
2022-02-09 07:09:18 -0300

    Created a visitor and messages for i18n.

--- pet-clinic-web/src/main/java/pet/clinic/config/DatabaseConfigurationService.java
+++ pet-clinic-web/src/main/java/pet/clinic/config/DatabaseConfigurationService.java
@@ -3,11 +3,18 @@ package pet.clinic.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import pet.clinic.datasource.FakeData;
+
+import java.util.Locale;
 
 //@PropertySource("classpath:datasource.properties") se comentar aqui ele vai rodar o valor do application.properties active
 @Configuration
-public class DatabaseConfigurationService {
+public class DatabaseConfigurationService implements WebMvcConfigurer {
 
     @Bean
     FakeData fakeData(@Value("${guru.username}") String username,
@@ -19,6 +26,25 @@ public class DatabaseConfigurationService {
         fakeData.setJdbc(jdbc);
 
         return fakeData;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(Locale.US);
+        return slr;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
     }
 
 }
--- pet-clinic-web/src/main/java/pet/clinic/model/Owner.java
+++ pet-clinic-web/src/main/java/pet/clinic/model/Owner.java
@@ -1,6 +1,17 @@
 package pet.clinic.model;
 
+import java.time.LocalDate;
+import java.util.Set;
+
 public class Owner extends Person{
 
+    private Set<Pet> pets;
 
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
